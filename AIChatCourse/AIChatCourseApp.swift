3bb7f842aa95bde1b9ee1aec662e663f5f04251d
Lesley Koopmans <lesleykoopmans@gmail.com>
2025-02-05 14:32:10 +0100

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Lesley Koopmans on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Lesley Koopmans on 05/02/2025.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
